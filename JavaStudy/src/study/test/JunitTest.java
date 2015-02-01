82b651404720592a1ee7fe2a4a209d7cc36fc36d
pstkiz <develok@naver.com>
2015-02-01 13:36:45 +0900

    second commit

---
+++ JavaStudy/src/study/test/JunitTest.java
@@ -0,0 +1,14 @@
+package study.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JunitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
